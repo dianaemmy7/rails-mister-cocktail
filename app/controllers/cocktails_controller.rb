315de666c81e6fa1a56218b8b3bd8a7e799eacc2
Diana Schoeller <diana.schoeller@student.unisg.ch>
2019-09-04 17:17:39 +0300

    yummmm

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,66 @@
+require "json"
+require 'open-uri'
+
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def edit
+    #set flat
+  end
+
+  def update
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  def destroy
+    @cocktail.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to cocktails_path
+  end
+
+  # def self.get_photo_url
+  #   url = "https://api.unsplash.com/search/photos?page=1&query=office"
+  #   url_serialized = open(url).read
+  #   repos = JSON.parse(url_serialized)
+  #   "url"
+  # end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:cocktail).permit(:name)
+  end
+end
+
+
+# CocktailsController.get_photo_url
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require 'open-uri'
+
+# require_relative "../app/models/post"
+
+# TODO: Write a seed to insert 10 posts in the database fetched from the Hacker News API.
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+url_serialized = open(url).read
+repos = JSON.parse(url_serialized)
+# => repos is an `Array` of `Hashes`.
+
+repos['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
