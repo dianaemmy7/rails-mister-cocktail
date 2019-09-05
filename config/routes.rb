10463197790050eab0e8323e1d6a05dec0fab35e
Diana Schoeller <diana.schoeller@student.unisg.ch>
2019-09-05 17:24:06 +0300

    route destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'cocktails#index'
-  resources :cocktails, only: [:show, :edit, :update, :new, :create] do
+  resources :cocktails, only: [:show, :edit, :update, :new, :create, :destroy] do
     resources :doses, only: [:new, :create]
     resources :reviews, only: [:create]
   end
